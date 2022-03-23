6937fcbbe953314a08a45635dec38adf7e5c412e
Nathalia-Urenha <naty.urenha@gmail.com>
2022-03-23 19:55:17 -0300

    ploting histroplotting histogram

--- 1-Preprocessing/DataFrequencyDistribution.py
+++ 1-Preprocessing/DataFrequencyDistribution.py
@@ -4,20 +4,20 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def main():
     # Faz a leitura do arquivo
     input_file = '0-Datasets/echocardiogramClear.data'
     names = ['survival','still-alive','age-at-heart-attack','pericardial-effusion','fractional-shortening', 'epss', 'lvdd', 'wall-motion-score', 'wall-motion-index', 'mult', 'group', 'alive-at-1']
-    df = pd.read_csv(input_file,    # Nome do arquivo com dados
-                     names = names) # Nome das colunas                      
-    # ShowInformationDataFrame(df,"Dataframe original")
+    
+    df = pd.read_csv(input_file, names = names)                     
 
-    ##Passos a serem implementados##
-
-    #Imprimir todos os atributos idade de pessoas que tem um ataque cardíaco
+    #Atributos idade de pessoas que tem um ataque cardíaco
     df_age = (df['age-at-heart-attack'])
-    #df_age.sort_values(ascending=True)
+    array_age = df_age.tolist()
+    print(array_age)
+    
 
     #Idade Mínima e Máxima que as pessoas que tem um ataque cardíaco
     age_min = int(df.min()[['age-at-heart-attack']])
@@ -28,7 +28,6 @@ def main():
 
     #Calcular a amplitude de classe
     range = ceil((age_max - age_min)/number_classes)
-    print(range)
 
 
 
@@ -39,14 +38,30 @@ def main():
         frequencias.append('{} - {}'.format(round(valor,1),round(valor+range,1)))
         valor += range
 
-    print(frequencias)
+    print('frequencias', frequencias)
 
     #Rotular os valores dos atributos de acordo com sua classe
-  
     freq_abs = pd.qcut(df_age,len(frequencias),labels=frequencias) # Discretização dos valores em k faixas, rotuladas pela lista criada anteriormente
-    print(pd.value_counts(freq_abs))
     print(freq_abs)
+    
+    #quantidade de atributos idade que tem em cada classe
+    qtd_atr = (pd.value_counts(freq_abs)) 
+    print('Quantidade de atributos em cada classe', qtd_atr)
 
+    #Histograma do atributo idade
+    bin = []
+    for number in frequencias:
+        bin.append(int(number[0:3]))
+
+    last_range = frequencias.pop()
+
+    bin.append(int(last_range[5:7]))
+
+    print(bin)
+    print(array_age)
+    plt.hist(array_age, bins=bin, edgecolor='black')
+    plt.show()
+    
 
 def ShowInformationDataFrame(df, message=""):
     print(message+"\n")
