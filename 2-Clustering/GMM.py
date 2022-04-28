d9a0793054a370d4c9bbcf6c2596aadb65b586c8
Nathalia Urenha <52292944+Nathalia-Urenha@users.noreply.github.com>
2022-04-28 13:54:40 -0300

    GMM

---
+++ 2-Clustering/GMM.py
@@ -0,0 +1,57 @@
+#Implementation of Kmeans from scratch and using sklearn
+#Loading the required modules 
+import numpy as np
+from scipy.spatial.distance import cdist 
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_samples
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plot_samples(projected, labels, title):    
+    fig = plt.figure()
+    u_labels = np.unique(labels)
+    for i in u_labels:
+        plt.scatter(projected[labels == i , 0] , projected[labels == i , 1] , label = i,
+                    edgecolor='none', alpha=0.5, cmap=plt.cm.get_cmap('tab10', 10))
+    plt.xlabel('component 1')
+    plt.ylabel('component 2')
+    plt.legend()
+    plt.title(title)
+
+def main():
+    #Dataset
+    input_file = '0-Datasets/echocardiogramClear.data'
+    names = ['survival','still-alive','age-at-heart-attack','pericardial-effusion','fractional-shortening', 'epss', 'lvdd', 'wall-motion-score', 'wall-motion-index', 'alive-at-1']
+    features = ['survival','still-alive','age-at-heart-attack','pericardial-effusion','fractional-shortening', 'epss', 'lvdd', 'wall-motion-score', 'wall-motion-index']
+    target = 'alive-at-1'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names)
+
+    #Load dataset Digits
+    data = df[features]
+    target = df[target]
+    
+    #Transform the data using PCA
+    pca = PCA(2)
+    projected = pca.fit_transform(data)
+    print(pca.explained_variance_ratio_)
+    print(data.shape)
+    print(projected.shape)    
+    plot_samples(projected, target, 'Original Labels') 
+    
+    #Applying sklearn GMM function
+    gm  = GaussianMixture(n_components=10).fit(projected)
+    print(gm.weights_)
+    print(gm.means_)
+    x = gm.predict(projected)
+
+    #Visualize the results sklearn
+    plot_samples(projected, x, 'Clusters Labels GMM')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
